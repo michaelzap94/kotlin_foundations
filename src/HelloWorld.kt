cf82b978c0fd6583c76e0bec4a9a1c3132817470
Michael Zapata <mikeyfriends@hotmail.com>
2020-09-07 18:53:22 +0100

    first commit

---
+++ src/HelloWorld.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    var str: String = "Hello world";
+    val age: Int = 23;
+    println(str + age);
+}
---
+++ src/conditionals/Conditionals.kt
@@ -0,0 +1,57 @@
+package conditionals
+
+fun main(args: Array<String>) {
+    var age: Int = 1;
+    //IF, ELSE IF, ELSE
+    if (age < 18) {
+        println("you cannot register");
+    } else if (age >= 18) {
+        println("you can register");
+    } else  {
+        println("unexpected");
+    }
+
+    //WHEN
+    when(age) {
+        1 -> println("first")
+        2 -> {
+            println("second")
+        }
+        3 -> println("third")
+        else -> {
+            //default:
+            println("default")
+        }
+    }
+
+    //Conditionals as Expressions:
+
+    // the result stored here, is always the LAST expression stored in each of this blocks,
+    // the block that matches the condition first, will be used.
+    //ONLY ONE BLOCK CAN -> BE ASSIGNED and/or BE EXECUTED
+    var position:String = when(++age) {
+        1 -> "first"
+        2,3 -> {//2 or 3
+            "this is not returned"
+            var anything = if (true) "anything" else "anything else"
+            var anything2 = if (false) {
+                "something"
+            } else {
+                "something else"
+            }
+            println(anything2)
+            "second"//IN Kotlin -> this is an expression
+            anything//IN Kotlin -> this is an expression too, the last element is returned in this block
+        }
+        3 * 2 -> "six"
+        //range:
+        in 1..10 -> { println("between 1 and 10"); "final"}//; semi-colon is used to separate expressions on the same line
+        !in 11..13 -> "not in 1 to 13"
+        is Int -> {println("is int"); "is int" }
+        else -> {
+            //default:
+            "default"
+        }
+    }
+    println(position)//anything
+}
