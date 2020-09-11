d1d0b6021328978fafda9a8c0581ba91b30b5c9c
Michael Zapata <mikeyfriends@hotmail.com>
2020-09-11 11:04:19 +0100

    objects singleton

---
+++ src/oop/ObjectsSingleton.kt
@@ -0,0 +1,14 @@
+package oop
+
+//Object Declaration -> Singleton: you can only have ONE INSTANCE of it
+//it can have properties, methods, etc,
+//You cannot create an object from it, since this is already an object.
+//Because a Kotlin object can't have any constructor, you can't pass any argument to it
+//However, you can have a init method for some initialization
+object Cache {
+    val someproperty = "any"
+    fun anyFunction(): String {return "anything"}
+    init {
+        println("init complete")
+    }
+}
